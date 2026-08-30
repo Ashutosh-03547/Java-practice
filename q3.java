8546fab26bee5278847cf5ec4bf7e47fa9536264
Ashutosh-03547 <pathyashutosh65@gmail.com>
2026-08-30 20:47:27 +0530

    new commit

--- q3.java
+++ q3.java
@@ -1,3 +1,35 @@
+
+import java.util.Scanner;
+
 public class q3 {
-    
+
+    static void transaction(double[] arr,int num) {
+        int sum=0;
+        for (int i = 0; i < num; i++) {
+
+            if (arr[i] == 0) {
+                continue;
+            } else if (sum > 10000) {
+                break;
+            } else {
+                sum += arr[i];
+            }
+
+        }
+      System.err.println("the total transaction amount is:"+ sum);
+    }
+    public static void main(String[] args) {
+        System.out.println("Enter all the transaction");
+        int num = 0;
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNextInt()) {
+            num = sc.nextInt();
+            break;
+        }
+        double[] arr = new double[num];
+        for (int i = 0; i < num; i++) {
+            arr[i] = sc.nextDouble();
+        }
+        transaction(arr, num);
+    }
 }
