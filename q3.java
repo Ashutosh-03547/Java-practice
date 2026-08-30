f4fb614caea0478200d4e34962e5213ed8cf9a9d
Ashutosh-03547 <pathyashutosh65@gmail.com>
2026-08-30 20:59:02 +0530

    update error

--- q3.java
+++ q3.java
@@ -9,27 +9,27 @@ public class q3 {
 
             if (arr[i] == 0) {
                 continue;
-            } else if (sum > 10000) {
-                break;
-            } else {
+            } else if (sum + arr[i] <= 10000) {
                 sum += arr[i];
+            } else {
+                break;
             }
 
         }
-      System.err.println("the total transaction amount is:"+ sum);
+      System.out.println("the total transaction amount is:"+ sum);
     }
     public static void main(String[] args) {
-        System.out.println("Enter all the transaction");
+        System.out.println("Enter number of transactions:");
         int num = 0;
         Scanner sc = new Scanner(System.in);
-        while (sc.hasNextInt()) {
-            num = sc.nextInt();
-            break;
-        }
+        num = sc.nextInt();
         double[] arr = new double[num];
+        System.out.println("Enter all the transaction amounts:");
         for (int i = 0; i < num; i++) {
             arr[i] = sc.nextDouble();
         }
         transaction(arr, num);
+
+        sc.close();
     }
 }
