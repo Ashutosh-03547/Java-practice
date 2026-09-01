68d84d23a6b96d86e1eae490ed3ddbdb08a2942c
Ashutosh-03547 <pathyashutosh65@gmail.com>
2026-09-01 21:25:56 +0530

    finding the  reverse

--- q5.java
+++ q5.java
@@ -3,16 +3,21 @@ import java.util.Scanner;
 public class q5 {
     static void reverse(int num) {
         int count = 0;
-        int rev=0;
-        while (num != 0) {
-            num /= 10;
-            count+=1;
+        int rev = 0;
+        int temp = num;
+
+        while (temp != 0) {
+            temp /= 10;
+            count++;
         }
-        for (int i = 0; i <= count; i++) {
+
+        for (int i = 0; i < count; i++) {
             int rem = num % 10;
-            rev = (rev * 10) + rem;
+            rev = rev * 10 + rem;
+            num /= 10;
         }
-        System.err.println("the reverse number is :"+rev);
+
+        System.out.println("The reverse number is: " + rev);
     }
     public static void main(String[] args) {
         System.out.println("enter the number");
