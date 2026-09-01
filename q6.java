fd976a22a6f02b807c61af6bb186ed18c920ddb0
Ashutosh-03547 <pathyashutosh65@gmail.com>
2026-09-01 22:23:31 +0530

    fibonacci corrected

--- q6.java
+++ q6.java
@@ -10,13 +10,17 @@ public class q6 {
         int num = sc.nextInt();
         int[] arr = new int[num];
         arr[0] = 0;
-        for (int i = 1; i < num; i++) {
+        arr[1] = 1;
+        for (int i = 2; i < num; i++) {
             nextnum = first + second;
             arr[i] = nextnum;
             first = second;
             second = nextnum;
 
         }
-        System.out.println("the fibonacci series are:"+arr);
+        System.out.println("the fibonacci series are:");
+        for (int i = 0; i < num; i++) {
+           System.out.println(arr[i]);            
+        }
     }
 }
