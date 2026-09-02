a7cb2c914bc596854b660668bf116b6c9cffcc46
Ashutosh-03547 <pathyashutosh65@gmail.com>
2026-09-02 23:57:31 +0530

    update error

--- q7.java
+++ q7.java
@@ -1,23 +1,29 @@
 import java.util.Scanner;
 
 public class q7 {
-    static int duplicate(int[] arr) {
+    static void duplicate(int[] arr) {
         int a = arr[0];
         int i=1;
-          while(i < 10) {
-            if (arr[i] != a) {
+        while (i < 5) {
+            if (arr[i] <= a) {
+                arr[i - 1] = arr[i];
+                arr[i] = a;
                 i++;
-                
+
             } else {
-                return arr[i];
+                a = arr[i];
+                i++;
             }
+        }
+        for (int j = 0; j < 5; j++) {
+            System.out.println(arr[j]);
         }
     }
     public static void main(String[] args) {
         System.out.println("Enter the numbers :");
         Scanner sc = new Scanner(System.in);
-        int[] arr = new int[10];
-        for (int i = 0; i < 10; i++) {
+        int[] arr = new int[5];
+        for (int i = 0; i < 5; i++) {
             arr[i] = sc.nextInt();
         }
         duplicate(arr);
