feac81e533aee742e51e0624ef714bffad4444c1
Ashutosh-03547 <pathyashutosh65@gmail.com>
2026-09-03 00:16:07 +0530

    update error

--- q7.java
+++ q7.java
@@ -3,15 +3,16 @@ import java.util.Scanner;
 public class q7 {
     static void duplicate(int[] arr) {
         int a = arr[0];
-        int i=1;
+        int i=0;
         while (i < 5) {
-            if (arr[i] <= a) {
-                arr[i - 1] = arr[i];
-                arr[i] = a;
+            if (arr[i] <= arr[i+1]) {
+                
                 i++;
 
             } else {
-                a = arr[i];
+                int temp = arr[i];
+                arr[i] = arr[i + 1];
+                arr[i + 1] = temp;
                 i++;
             }
         }
@@ -22,7 +23,7 @@ public class q7 {
     public static void main(String[] args) {
         System.out.println("Enter the numbers :");
         Scanner sc = new Scanner(System.in);
-        int[] arr = new int[5];
+        int[] arr = new int[6];
         for (int i = 0; i < 5; i++) {
             arr[i] = sc.nextInt();
         }
