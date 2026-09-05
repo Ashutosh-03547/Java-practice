555be5574fd838634073d390107d36a6958a9056
Ashutosh-03547 <pathyashutosh65@gmail.com>
2026-09-05 23:59:41 +0530

    string code

--- q11.java
+++ q11.java
@@ -2,15 +2,17 @@ public class q11 {
     public static boolean palindrome(String s) {
         int left = 0;
         int right = s.length() - 1;
-        while (left<right) {
+        while (left < right) {
             if (s.charAt(left) != s.charAt(right)) {
                 return false;
             }
             left++;
             right--;
         }
+        return true;
     }
     public static void main(String[] args) {
-        
+        String s = "abba";
+        System.out.println(isPalindrome(s));
     }
 }
