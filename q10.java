ac9cb509fe010d6a905d01599496b68749cf2fa8
Ashutosh-03547 <pathyashutosh65@gmail.com>
2026-09-05 23:54:05 +0530

    string code

--- q10.java
+++ q10.java
@@ -1,8 +1,11 @@
 public class q10 {
+    public static boolean strsame(String s1, String s2) {
+        return s1.equals(s2);
+    }
     public static void main(String[] args) {
         String s1 = "abc";
-        String s2 = "abcd";
-        if (s1 == s2) {
+        String s2 = "abc";
+        if (strsame(s1, s2)) {
             System.out.println("same string");
             
         } else {
