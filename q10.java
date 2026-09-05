376fef252fd20ef07d51801e76e8bbb914ab6886
Ashutosh-03547 <pathyashutosh65@gmail.com>
2026-09-05 23:49:51 +0530

    string code

---
+++ q10.java
@@ -0,0 +1,12 @@
+public class q10 {
+    public static void main(String[] args) {
+        String s1 = "abc";
+        String s2 = "abcd";
+        if (s1 == s2) {
+            System.out.println("same string");
+            
+        } else {
+            System.out.println("not same string");
+        }
+    }
+}
--- q9.java
+++ q9.java
@@ -16,5 +16,6 @@ public class q9 {
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
         }
+        
     }
 }
